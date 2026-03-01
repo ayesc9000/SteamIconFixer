3509c392295716c016f07ad11355624186efa0a6
ayesc9000 <ayesc@ayesc.ca>
2026-03-01 18:08:10 -0500

    Add help info to build script

--- build.cmd
+++ build.cmd
@@ -9,4 +9,6 @@ exit
 
 :error
 echo ----- Build failed -----
+echo If you got an unrecognized command error, make sure your Python scripts directory is in your PATH. Refer to the pyinstaller documentation for more information:
+echo https://pyinstaller.org/en/stable/installation.html#pyinstaller-not-in-path
 exit
