e0485b3267c265328349cd8305101977f0880fbc
ayesc9000 <ayesc@ayesc.ca>
2026-03-01 18:01:50 -0500

    Tidy build script

--- build.cmd
+++ build.cmd
@@ -3,14 +3,12 @@
 echo ----- Starting build -----
 
 pyinstaller --onefile sif.py
-
 if %ERRORLEVEL% neq 0 goto :error
-goto :complete
-
-:complete
 echo ----- Build completed -----
 exit
 
 :error
 echo ----- Build failed -----
 exit
+> [!NOTE]
+> Quotations are only necessary if the path contains spaces.
